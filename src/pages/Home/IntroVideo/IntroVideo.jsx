7dbda2f759accdfe0983f3f6fe1446c8a57a525f
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2024-12-29 17:22:36 +0600

    feat: video responsive issue fixed

--- src/pages/Home/IntroVideo/IntroVideo.jsx
+++ src/pages/Home/IntroVideo/IntroVideo.jsx
@@ -2,14 +2,29 @@ import ReactPlayer from "react-player";
 
 export default function IntroVideo() {
   return (
-    <section className="flex flex-col items-center justify-center py-10 md:py-20">
+    <section className="flex flex-col items-center justify-center px-5 py-10 md:py-20">
       <h2 className="text-center text-4xl font-medium text-primary lg:text-5xl">
         Discover Pensaki in Action
       </h2>
       <p className="mb-12 mt-4 text-center text-lg text-accent">
         Watch & Explore the features how we can boost your productivity
       </p>
-      <ReactPlayer controls url="https://www.youtube.com/watch?v=CG0IyuqaeMQ" />
+
+      <div className="md:hidden">
+        <ReactPlayer
+          controls
+          width={360}
+          height={203}
+          url="https://www.youtube.com/watch?v=CG0IyuqaeMQ"
+        />
+      </div>
+
+      <div className="hidden md:block">
+        <ReactPlayer
+          controls
+          url="https://www.youtube.com/watch?v=CG0IyuqaeMQ"
+        />
+      </div>
     </section>
   );
 }
